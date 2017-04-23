54b0f7ca984e1a74c8a157adfc0110bf069dcf41
giszhangke <giszhangke@qq.com>
2017-04-23 10:35:41 +0800

    重新上传FileTest.java

---
+++ 15/15_1/FileTest.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class FileTest {
+	public static void main(String[] args) throws IOException {
+		File file = new File(".");
+		// 直接获取文件名
+		// 获取绝对路径
+		// 获取上一级路径
+		// 在当前路径下创建一个临时文件
+		// 指定当JVM退出时删除该文件
+		// 以系统当前时间作为新文件名创建新文件
+		// 以指定的newFile对象来创建一个文件
+		// 以newFile对象来创建一个目录，因为newFile已经存在，所以无法用来创建目录
+		// 使用list()方法列出当前路径下的所有文件和路径
+	}
+}
+
